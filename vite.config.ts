cb01c4bb9f0a20a223d816d929dabbb5da5148bf
joeswagson <uranousbridge@gmail.com>
2026-02-19 13:35:45 -0800

    Bind configs.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BIND_TO_PUBLIC = true;
+const PORT = 5173
+
+const BIND_ADDRESS = BIND_TO_PUBLIC ? "0.0.0.0" : "127.0.0.1";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        bind_address: BIND_ADDRESS,
+        port: PORT
+    },
+    plugins: [react()],
 })
