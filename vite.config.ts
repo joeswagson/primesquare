c4713dfbd3f70991a40fd25f1948cc9d5cb297cb
joeswagson <uranousbridge@gmail.com>
2026-02-19 17:21:43 -0800

    Fix host configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const BIND_ADDRESS = BIND_TO_PUBLIC ? "0.0.0.0" : "127.0.0.1";
 // https://vite.dev/config/
 export default defineConfig({
     server: {
-        bind_address: BIND_ADDRESS,
+        host: BIND_ADDRESS,
         port: PORT
     },
     plugins: [react()],
